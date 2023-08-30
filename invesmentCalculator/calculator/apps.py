e70dcafb8ee9bc05c4b948ca3cd5564f77ba578d
Ahmet Harun Ertürk <is_ahmet@outlook.com>
2023-08-30 15:24:14 +0200

    Invesments

--- invesmentCalculator/calculator/apps.py
+++ invesmentCalculator/calculator/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DcaAppConfig(AppConfig):
+class CalculatorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dca_app'
+    name = 'calculator'
